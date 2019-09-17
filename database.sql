627de25f27fcb779471cbe2ec9a3bf02ac331303
Luke Becker <beckerluke1@gmail.com>
2019-09-17 17:01:04 -0500

    setup basic database and named it in pool

--- database.sql
+++ database.sql
@@ -6,5 +6,21 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "type" VARCHAR (100) NOT NULL,
+    "email" VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE "deals" (
+	"id" serial primary key,
+	"start_time" timestamp,
+	"end_time" timestamp,
+	"description" varchar(100) not null,
+	"location" varchar(255) not null
+);
+
+CREATE TABLE "user_deals" (
+	"id" serial primary key,
+	"user_id" INT REFERENCES "user",
+	"deals_id" INT REFERENCES "deals"
 );
