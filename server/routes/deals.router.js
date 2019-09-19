5b189fe017155d2871e56426d55307b319abfbd1
Luke Becker <beckerluke1@gmail.com>
2019-09-19 14:00:54 -0500

    Changed query for GET in deals router to join tables and get business name. Displayed all deals in database on the landing page

--- server/routes/deals.router.js
+++ server/routes/deals.router.js
@@ -6,7 +6,9 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    let queryText = `SELECT * FROM "deals" ORDER BY "end_time";`;
+    let queryText = `SELECT * FROM "deals" 
+                    JOIN "user" ON "user".id = "deals"."user_id"
+                    ORDER BY "end_time";`;
     pool.query(queryText).then(result => {
       // Sends back the results in an object
       res.send(result.rows);
