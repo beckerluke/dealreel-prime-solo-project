1133ba4273639c8faf294dd376cadb1a29e78ebe
Luke Becker <beckerluke1@gmail.com>
2019-09-26 21:10:59 -0500

    Adjusted query for Get req in deals router

--- server/routes/deals.router.js
+++ server/routes/deals.router.js
@@ -9,7 +9,9 @@ const moment = require('moment');
 router.get('/', (req, res) => {
     let queryText = `SELECT * FROM "deals" 
                     JOIN "user" ON "user".id = "deals"."user_id"
-                    WHERE "end_time" >= $1 AND "end_time" <= $2
+                    WHERE "start_time" <= $1 
+                    AND "end_time" >= $1
+                    AND "end_time" <= $2
                     ORDER BY "user"."id", "end_time" DESC;`;
 
     const currentDateTime = moment().format();
